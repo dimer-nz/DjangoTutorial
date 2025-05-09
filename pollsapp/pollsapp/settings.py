caec31f82ad6d76f60e585ba5c845154bbcf9d6a
Nick Zagavieros <n.zagavieros@dimersoftware.com>
2025-05-09 13:50:54 +0300

    add real email sending functinality

--- pollsapp/pollsapp/settings.py
+++ pollsapp/pollsapp/settings.py
@@ -129,6 +129,13 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email handling
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "n.zagavieros@dimersoftware.com"
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" 
 NOTIFY_EMAIL = "n.zagavieros@dimersoftware.com"
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nikoszagavieros@gmail.com'
+EMAIL_HOST_PASSWORD = 'czpw ebey hpcp mfxy'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
