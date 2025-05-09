91186184f70f1c855e0b92f8e695718612ae59b1
Nick Zagavieros <n.zagavieros@dimersoftware.com>
2025-05-09 11:04:21 +0300

    create contact app and add to settings

--- pollsapp/pollsapp/settings.py
+++ pollsapp/pollsapp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'contact.apps.ContactConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
